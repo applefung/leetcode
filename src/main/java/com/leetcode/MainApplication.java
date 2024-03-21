8a4ffe936491ddc2a746bd1bafe2fe48b31af051
amber.fung <amber.fung@lalamove.com>
2024-03-21 23:10:14 +0800

    feat: add findTheIndexOfTheFirstOccurrenceInAString

--- src/main/java/com/leetcode/MainApplication.java
+++ src/main/java/com/leetcode/MainApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
-		RemoveDuplicates x = new RemoveDuplicates();
+		FindTheIndexOfTheFirstOccurrenceInAString x = new FindTheIndexOfTheFirstOccurrenceInAString();
 
-		int size = x.removeDuplicates(new int[]{1,1,2});
+		int size = x.strStr("mississippi", "issip");
 		System.out.println("result==>"+size);
 		SpringApplication.run(MainApplication.class, args);
 	}

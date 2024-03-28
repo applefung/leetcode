03fd54ecf1b65cae96817f399f0431d841feb2a6
amber.fung <amber.fung@lalamove.com>
2024-03-28 15:17:02 +0800

    feat: add SortedArrayToBST

---
+++ src/main/java/com/leetcode/SortedArrayToBST.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class SortedArrayToBST {
+    public TreeNode createBST(int[] nums, int left, int right) {
+        if(left>right) {
+            return null;
+        }
+
+        int mid = left + (right - left)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = createBST(nums, left, mid-1);
+        root.right = createBST(nums, mid+1, right);
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return createBST(nums, 0, nums.length-1);
+    }
+}

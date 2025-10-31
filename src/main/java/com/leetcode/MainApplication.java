e1b586528c3a4b9b088a81d7943de3f7aadf28c4
applefung <fungiptsing@gmail.com>
2025-10-31 20:20:24 +0800

    feat: add moveZeroes

--- src/main/java/com/leetcode/MainApplication.java
+++ src/main/java/com/leetcode/MainApplication.java
@@ -6,17 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
-		// Build linked list 1 -> 2 -> 2 -> 1
-		ListNode n1 = new ListNode(1);
-		ListNode n2 = new ListNode(2);
-		ListNode n3 = new ListNode(2);
-		ListNode n4 = new ListNode(1);
-		n1.next = n2;
-		n2.next = n3;
-		n3.next = n4;
-
-		PalindromeLinkedList checker = new PalindromeLinkedList();
-		System.out.println(checker.isPalindrome(n1));
+		MoveZeroes moveZeroes = new MoveZeroes();
+		moveZeroes.moveZeroes2(new int[]{1,0,1});
 		// SpringApplication.run(MainApplication.class, args);
 	}
 
---
+++ src/main/java/com/leetcode/MoveZeroes.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i]==0) {
+                for(int j=i; j<nums.length; j++) {
+                    if(nums[j] != 0){
+                        nums[i]=nums[j];
+                        nums[j]=0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int slow = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]!=0){
+                nums[slow] = nums[i];
+                if(slow != i) {
+                    nums[i]=0;
+                }
+                slow++;
+            }
+        }
+    }
+}

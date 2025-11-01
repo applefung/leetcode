8da1ce945ebcf023b1f3ae88c0ba3f7d23ef386f
applefung <fungiptsing@gmail.com>
2025-11-01 20:15:59 +0800

    feat: add TwoSum4InputIsABst

---
+++ src/main/java/com/leetcode/TwoSum4InputIsABst.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+import java.util.HashSet;
+
+public class TwoSum4InputIsABst {
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, k, set);
+    }
+
+    private boolean dfs(TreeNode root, int k, HashSet<Integer> set) {
+        if(root == null) {
+            return false;
+        }
+        if(set.contains(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return dfs(root.left, k, set) || dfs(root.right, k, set);
+    }
+}
